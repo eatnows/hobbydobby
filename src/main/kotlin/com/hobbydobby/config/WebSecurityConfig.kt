b3ad617ea9d9932b8c48a04e193438e06db70c5d
hil00137 <hil00137@gmail.com>
2020-05-05 20:35:13 +0900

    SecurityConfig의 이름 변경 (MethodSecurityConfig 를 추가하기 위함). fixed #1

--- src/main/kotlin/com/hobbydobby/config/WebSecurityConfig.kt
+++ src/main/kotlin/com/hobbydobby/config/WebSecurityConfig.kt
@@ -7,7 +7,7 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 
 @Configuration
 @EnableWebSecurity
-class SecurityConfig : WebSecurityConfigurerAdapter() {
+class WebSecurityConfig : WebSecurityConfigurerAdapter() {
     override fun configure(http : HttpSecurity) {
         http.csrf().ignoringAntMatchers("/gui","/graphql").and()
                 .authorizeRequests()
